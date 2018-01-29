ab38de2852e06e08d38baec7dab649a8636da3da
Schogan <lrschanbacher@gmail.com>
2018-01-29 13:59:35 -0600

    With comments

--- CIS2013_Week02_Homework1.cpp
+++ CIS2013_Week02_Homework1.cpp
@@ -10,16 +10,17 @@ int main(){
 	bool keep_running = true;
 	
 	while (keep_running){
-	
+	// request for input and inputs from user
 		cout << "Please enter your first number: " << endl;
 		cin >> first_number;
 		cout << "Please enter your second number: " << endl;
 		cin >> second_number;
+		// user selects the action to do
 		cout << "What would you like to do? Please enter one of the following: " << endl <<
 			"+" << endl << "-" << endl << "*" << endl << "/" << endl;
 		cin >> action;
 	
-	
+	//where the action actually takes place on the users information
 		if (action == '+'){
 			cout << "Your answer is: ";
 			cout << first_number + second_number << endl;
@@ -39,7 +40,7 @@ int main(){
 			cout << "Your answer is: ";
 			cout << first_number / second_number << endl;
 		}
-		
+		// user decides to continue or not
 		cout << "Do you want to keep running? Enter y or n" << endl;
 		cin >> question;
 		if (question != 'y'){keep_running = false;}
