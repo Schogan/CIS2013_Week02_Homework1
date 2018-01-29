f5f38825c8a3c9fd148a25fb3ec67c8c2e29c0f5
Schogan <lrschanbacher@gmail.com>
2018-01-29 13:26:06 -0600

    Complete Calculator

---
+++ CIS2013_Week02_Homework1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int first_number = 1;
+	int second_number = 1;
+	char action;
+	char question;
+	bool keep_running = true;
+	
+	while (keep_running){
+	
+		cout << "Please enter your first number: " << endl;
+		cin >> first_number;
+		cout << "Please enter your second number: " << endl;
+		cin >> second_number;
+		cout << "What would you like to do? Please enter one of the following: " << endl <<
+			"+" << endl << "-" << endl << "*" << endl << "/" << endl;
+		cin >> action;
+	
+	
+		if (action == '+'){
+			cout << "Your answer is: ";
+			cout << first_number + second_number << endl;
+		}
+		
+		if (action == '-'){
+			cout << "Your answer is: ";
+			cout << first_number - second_number << endl;
+		}
+		
+		if (action == '*'){
+			cout << "Your answer is: ";
+			cout << first_number * second_number << endl;
+		}
+		
+		if (action == '/'){
+			cout << "Your answer is: ";
+			cout << first_number / second_number << endl;
+		}
+		
+		cout << "Do you want to keep running? Enter y or n" << endl;
+		cin >> question;
+		if (question != 'y'){keep_running = false;}
+	}
+}
